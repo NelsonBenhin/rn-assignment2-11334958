df9664f5224ebf959d29e8e1f7bfaa648a011a4b
NelsonBenhin <nbenhin@st.ug.edu.gh>
2024-06-10 23:39:50 +0400

    Created the backgorund colour

--- MyAPP/App.js
+++ MyAPP/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        My name is <Text style={styles.name}>Nelson Benhin</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#04d89e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24, // Removed quotes around 24 to make it a number
+  },
+  name: {
+    fontWeight: 'bold',
   },
 });
